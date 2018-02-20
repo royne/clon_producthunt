f5e8ddf3ad291ad30e7b3e3c94477df94821f8f9
royne <royne.chaves@gmail.com>
2018-02-19 21:05:28 -0500

    crea ruta y formulario de nuevo prodcuto

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'products/index'
-
   get '/products', to: 'products#index'
+  get 'products/new', to: 'products#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
